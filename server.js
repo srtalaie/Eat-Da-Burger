011a214c380746611f1f50a9d32715b027b6f201
srtalaie <srt4721@yahoo.com>
2018-08-15 09:33:59 -0700

    Fixing app connection with heroku

--- server.js
+++ server.js
@@ -32,9 +32,11 @@ connection.connect(function (err) {
         return;
     }
     // Start our server so that it can begin listening to client requests.
-    app.listen(PORT, function () {
-        // Log (server-side) when our server has started
-        console.log("Server listening on: http://localhost:" + PORT);
-        console.log("connected as id " + connection.threadId);
-    });
+    console.log("connected as id " + connection.threadId);
+
+});
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+
 });
