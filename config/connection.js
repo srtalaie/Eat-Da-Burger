41f918ca5bddad64f534aa6ac6804475160f02a6
srtalaie <srt4721@yahoo.com>
2018-08-15 09:45:27 -0700

    Fixing app connection with heroku

--- config/connection.js
+++ config/connection.js
@@ -8,5 +8,15 @@ const connection = mysql.createConnection({
     password: 'UOBH#:O*(YO9p3874guo',
     database: "burger_db"
   });
+  
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+  }
+  // Start our server so that it can begin listening to client requests.
+  console.log("connected as id " + connection.threadId);
 
+});
   module.exports = connection;
--- server.js
+++ server.js
@@ -25,16 +25,6 @@ routes.post(app);
 routes.put(app);
 routes.delete(app);
 
-// Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    // Start our server so that it can begin listening to client requests.
-    console.log("connected as id " + connection.threadId);
-
-});
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
