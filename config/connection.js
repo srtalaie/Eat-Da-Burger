a659e880906d9a470bbd6563f6332188f0bc60f6
srtalaie <srt4721@yahoo.com>
2018-08-15 09:36:47 -0700

    Fixing app connection with heroku

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: 'UOBH#:O*(YO9p3874guo',
+    password: pass.password,
     database: "burger_db"
   });
 
---
+++ config/password.js
@@ -0,0 +1,3 @@
+let pass = 'UOBH#:O*(YO9p3874guo';
+
+module.exports = { password: pass };
