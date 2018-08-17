13d2fea614df19809bec01d4210751ba84768d93
srtalaie <srt4721@yahoo.com>
2018-08-16 18:24:14 -0700

    DB working

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql');
+let connection = {};
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "admin",
     password: 'UOBH#:O*(YO9p3874guo',
     database: "burger_db"
   });
+}
 
 module.exports = connection;
